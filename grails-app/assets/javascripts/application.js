9bd3686948ff70b9871ccc7230e8575010498b31
Søren Berg Glasius <soeren@glasius.dk>
2019-05-13 13:28:54 +0200

    MVP demo

--- grails-app/assets/javascripts/application.js
+++ grails-app/assets/javascripts/application.js
@@ -5,7 +5,12 @@
 // You're free to add application-wide JavaScript to this file, but it's generally better
 // to create separate JavaScript files as needed.
 //
-//= require jquery-3.3.1.min
-//= require bootstrap
+//= require jquery/jquery.js
+//= require bootstrap/js/bootstrap.js
+//= require jquery-easing/jquery.easing.js
+//= require startbootstrap-sb-admin-2/js/sb-admin-2.js
+//= require chart.js/Chart.js
+//= require startbootstrap-sb-admin-2/demo/js/chart-area-demo.js
+//= require startbootstrap-sb-admin-2/demo/js/chart-pie-demo.js
 //= require popper.min
 //= require_self
